a7e78af19facceb2e4cb3b6e95741c46902a5b6f
BEANSTASTEGOODALT <hudsonhippo123@gmail.com>
2026-08-12 16:04:37 +1200

    modified:   index.js

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 require("dotenv").config();
 
-const { App } = require("@slack/bolt");
+const {
+    App
+} = require("@slack/bolt");
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true
 });
 
-app.command("/slacky-ping", async ({ command, ack, respond }) => {
-  const start = Date.now();
-  await ack();
-  const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+app.command("/slacky-ping", async ({
+    command,
+    ack,
+    respond
+}) => {
+    const start = Date.now();
+    await ack();
+    const latency = Date.now() - start;
+    await respond({
+        text: `Pong!\nLatency: ${latency}ms`
+    });
+});
+
+app.command("/slacky-ask", async ({
+    command,
+    ack,
+    respond
+}) => {
+    await ack();
+    let question = command.text.trim().split(' ')[0];
+    let response = await fetch(
+        "https://integrate.api.nvidia.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.API_KEY}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                model: "meta/llama-3.2-3b-instruct",
+                messages: [{
+                    role: "user",
+                    content: question
+                }],
+            })
+        }
+    );
+    let data = await response.json();
+        await respond({
+        text: `Slacky says:\n${data.choices[0].message.content}`
+    });
 });
 
 (async () => {
-  await app.start();
-  console.log("bot is running!");
+    await app.start();
+    console.log("bot is running!");
 })();
